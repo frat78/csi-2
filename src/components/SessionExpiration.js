ec5c72d40212bbfb967f10182efb12ba657b19a6
frat78 <kfraticelli@aaski.com>
2017-12-17 12:29:22 -0500

    added proper redirect code to the session expiration logic

--- src/components/SessionExpiration.js
+++ src/components/SessionExpiration.js
@@ -2,24 +2,37 @@ import React, {
   Component
 } from 'react';
 import styled from 'styled-components';
+import { Redirect } from 'react-router';
 
 class SessionExpiration extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      redirect: false
+    };
   }
 
   render(){
+
+    const { redirect } = this.state;
+    if (redirect) {
+      //blow away the expired session
+      delete sessionStorage.sessionKey;
+      delete sessionStorage.CSI_username;
+      //then redirect to the login page
+      return <Redirect to='/'/>;
+    }
 
     const RedirectionDiv = styled.div`
       margin-top: 50px;
     	border-top: 1px solid #eee;
     	padding: 20px 0;
-    	font-size: 12px;
-    	color: #999;
+    	font-size: 22px;
+    	color: #000;
     `;
 
     var timer = setTimeout(function() {
-            window.location='/'
+            this.setState({ redirect: true })
         }, 3000);
 
 
